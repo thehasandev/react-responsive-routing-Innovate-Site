0c14ede31553d2e5c0931d8aaea6e84634502058
Hasan <thehasandev@gmail.com>
2023-08-31 22:00:25 +0600

    error

--- src/Route/Error.jsx
+++ src/Route/Error.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Container from '../components/Container'
+
+import { Link } from 'react-router-dom'
 
 function Error() {
   return (
-    <div>
-      <h1 className='text-large text-secondary'>Page Error 404...</h1>
-    </div>
+    <section className='py-20'>
+      <Container>
+          <h1 className='text-7xl font-sans font-extraBold mb-8 text-black'>Page Error 404</h1>
+          <Link to="/">
+            <button className='bg-gray-700 text-lg text-white py-4 px-8 rounded-lg'>Goto Home Page</button>
+          </Link>
+      </Container>
+    </section>
   )
 }
 
